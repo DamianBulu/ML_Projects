450a7faa299fd8930adf0d5e2b8d6e9af04319bf
DamianBulu <dambulumn@gmail.com>
2025-08-09 19:47:08 +0300

    Adaugare fisiere proiect

--- data_loader.py
+++ data_loader.py
@@ -1,16 +1,26 @@
-from sklearn.datasets import load_digits
-import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+import pandas as pd
 
-def load_data():
-    data = load_digits()
-    return data.data, data.target
 
-def plot_sample_images(X, y, n=10):
-    plt.figure(figsize=(10, 2))
-    for i in range(n):
-        plt.subplot(1, n, i + 1)
-        plt.imshow(X[i].reshape(8, 8), cmap='gray')
-        plt.title(f"Label: {y[i]}")
-        plt.axis("off")
-    plt.tight_layout()
-    plt.show()
+def load_data(type_of_scale=None,test_size=0.2,val_size=0.2,random_state=42):
+    #Incarcare dataset
+    iris=load_iris()
+
+    #Extrage caracteristici(features) si etichete(target)
+    X=iris.data
+    y=iris.target
+
+    feature_names=iris.feature_names
+    target_names=iris.target_names
+
+    #Impartire date seturi de antrenament,validare si test
+    X_train_val,X_test,y_train_val,y_test=train_test_split(X,y,test_size=test_size,random_state=random_state)
+    val_size_relative=val_size/(1-test_size)
+    X_train,X_val,y_train,y_val=train_test_split(X_train_val,y_train_val,test_size=val_size_relative,random_state=random_state)
+
+    return X_train,X_val,X_test,y_train,y_val,y_test,feature_names,target_names
+
+
+
--- model.py
+++ model.py
@@ -1,22 +1,42 @@
-from sklearn.mixture import GaussianMixture
+from collections import Counter
 from sklearn.metrics import accuracy_score
+from sklearn.utils.validation import check_is_fitted,check_X_y,check_array
+from sklearn.base import BaseEstimator,ClassifierMixin
 import numpy as np
 
-def train_gmm(X, n_components=10):
-    gmm = GaussianMixture(n_components=n_components, random_state=42)
-    gmm.fit(X)
-    return gmm
+class KNN(BaseEstimator,ClassifierMixin):
+    def __init__(self,k=3,distance_metric='euclidean'):
+        self.k=k
+        self.distance_metric=distance_metric
+    def compute_distance(self,a,b):
+        if self.distance_metric=='euclidean':
+            return np.sqrt(np.sum((a-b)**2))
+        elif self.distance_metric=='manhattan':
+            return np.sum(np.abs(a-b))
+        else:
+            raise ValueError("Invalid distance metric. Choose 'euclidean' or 'manhattan'")
 
-def predict_labels(gmm, X, true_labels):
-    cluster_labels = gmm.predict(X)
+    def fit(self,X,y):
+        X,y=check_X_y(X,y)
+        self.X_train=X
+        self.y_train=y
+        self.classes_=np.unique(y)
+        return self
+    def predict_one(self,x):
+        distances=[self.compute_distance(x,x_train) for x_train in self.X_train]
+        k_indices=np.argsort(distances)[:self.k]
+        k_nearest_labels=[self.y_train[i] for i in k_indices]
+        most_common=Counter(k_nearest_labels).most_common(1)
+        return most_common[0][0]
 
-    # Aliniază cluster-urile cu etichetele reale
-    aligned_labels = np.zeros_like(cluster_labels)
-    for i in range(gmm.n_components):
-        mask = (cluster_labels == i)
-        if np.sum(mask) == 0:
-            continue
-        aligned_labels[mask] = np.bincount(true_labels[mask]).argmax()
+    def predict(self,X):
+        check_is_fitted(self) #Verify fit() has been called
+        X=check_array(X)
+        predictions=[self.predict_one(x) for x in X]
+        return np.array(predictions)
+    def evaluate(self,X,y):
+        y_pred=self.predict(X)
+        return accuracy_score(y,y_pred)
+    def score(self,X,y):
+        return accuracy_score(y,self.predict(X))
 
-    acc = accuracy_score(true_labels, aligned_labels)
-    return aligned_labels, acc
